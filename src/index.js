bcf01b0e1bc627d1cae351f68fb7426f11e77f3d
Ofri Dagan <ofridagan@gmail.com>
2020-06-17 21:18:23 +0300

    display the variable names

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import './styles/index.css';
 
 import * as tutiCode from './tuti.js';
 
-Object.keys(tutiCode).forEach((fn) => {
-    const result = tutiCode[fn]();
+Object.keys(tutiCode).forEach((key) => {
+    const result = tutiCode[key] instanceof Function ? tutiCode[key]() : tutiCode[key];
 
     const resultDiv = document.createElement('div');
     resultDiv.className = "result";
+
+    const label = document.createTextNode(key);
+    label.className = "label";
 
     const resultText = document.createTextNode(result);
     resultDiv.appendChild(resultText);
 
     const resultsContainer = document.getElementById("results");
 
+    resultsContainer.appendChild(label);
     resultsContainer.append(resultDiv);
 });
--- src/tuti.js
+++ src/tuti.js
@@ -1,7 +1,27 @@
-export const aaa = () => 'I Love You';
+/*
+ * ########################################  
+ *                 tuti.js
+ * ########################################  
+ *
+ *  Every value/function you export in this file will be displayed on the page...
+ *  Have Fun!
+ */
 
-export const bbb = () => {
-    return 'Tuti';
+
+
+
+// ----------------------------------------  
+// Examples (add your code above so it will be on top)
+// ----------------------------------------  
+
+// just a string value
+export const first = "I Love You";
+
+// a function with no arguments the just return value
+export const second = () => "Tuti";
+
+// a function with a body
+export const third = () => {
+    const fiveVery = [1,2,3,4,5].map(() => "Very");
+    return fiveVery.join(" ") + " Muchy";
 }
-
-export const ccc = () => 'Very much!';
